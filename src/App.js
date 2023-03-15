1f5f406b00f3fb797adc3320d1520559027c6610
vladoslatinos <116588242+vladoslatinos@users.noreply.github.com>
2023-03-16 01:21:56 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 
+import React, { useState } from 'react'; 
 import Button from './Button';
 
-const App = () => {
 
-  return (
-    <div>
-      <Button text={'Vlad'}/>
-    </div>
-  )
-}
-
-
+function App() { 
+  const [count, setCount] = useState(0); 
+ 
+  function incrementCount() { 
+    setCount(count + 1); 
+  } 
+ 
+  function decrementCount() { 
+    setCount(count - 1); 
+  } 
+ 
+  return ( 
+    <div> 
+      <h1>Count: {count}</h1> 
+      <Button onClick={incrementCount}>Increment</Button> 
+      <Button onClick={decrementCount}>Decrement</Button> 
+    </div> 
+  ); 
+} 
+ 
 export default App;
