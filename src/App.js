d757c25e77198f20977475e83dbd114016bfaece
vladoslatinos <116588242+vladoslatinos@users.noreply.github.com>
2023-03-20 12:59:23 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ function App() {
   const [data, setData] = useState([]); 
  
   useEffect(() => { 
-    fetch('https://jsonplaceholder.typicode.com/posts') 
-      .then(response => response.json()) 
-      .then(json => setData(json)); 
+    const fetchData = async () => { 
+      const response = await fetch('https://jsonplaceholder.typicode.com/posts'); 
+      const json = await response.json(); 
+      setData(json); 
+    }; 
+    fetchData(); 
   }, []); 
  
   return ( 
